2bf7fbb9c69c6c42009b6b238f57784f38b5c06b
DolphyXu <390737082@qq.com>
2018-08-02 16:36:29 +0800

    remove unuseful info

--- dolphy/controller/PubClient.php
+++ dolphy/controller/PubClient.php
@@ -11,10 +11,6 @@
 class PubClient extends MQTTClient
 {
     public function pub() {
-//        $this->client->onConnect('connect');
-//        $this->client->onDisconnect('disconnect');
-//        $this->client->connect("localhost", 1883, 5);
-
         $this->client->publish($this->topic, $this->message . date('Y-m-d H:i:s'), 1, 0);
 
         $this->client->disconnect();
--- dolphy/controller/SubClient.php
+++ dolphy/controller/SubClient.php
@@ -11,16 +11,10 @@ include 'MQTTClient.php';
 class SubClient extends MQTTClient
 {
     public function sub() {
-//        $this->client->onConnect('connect');
-//        $this->client->onDisconnect('disconnect');
-//        $this->client->onSubscribe('subscribe');
-//        $this->client->onMessage('message');
-
         $this->client->subscribe($this->topic, 1);
         while (true) {
             $this->client->loop();
         }
-
 
         $this->client->disconnect();
         unset($this->client);
